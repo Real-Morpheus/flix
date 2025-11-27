8646eb92f8943190665dd5621caf9d2af8056267
Real-Morpheus <skvishwakarma583@gmail.com>
2025-11-27 16:11:13 +0530

    Add files via upload

---
+++ providers/4khdhub/catalog.ts
@@ -0,0 +1,20 @@
+export const catalog = [
+  {
+    title: "Home",
+    filter: "",
+  },
+  {
+    title: "Latest TV Shows",
+    filter: "/category/new-series-10811",
+  },
+  {
+    title: "Anime",
+    filter: "/category/anime-10812",
+  },
+  {
+    title: "4K HDR",
+    filter: "/category/4k-hdr-10776",
+  },
+];
+
+export const genres = [];
